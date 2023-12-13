c719483abe699fdb2d719256661ac1f90046620d
Gustavo-Rodrigues <gustavodasilvama10@gmail.com>
2023-12-13 14:34:41 -0300

    Class Reader Criada

---
+++ Reader.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs/promises")
+
+class Reader{
+    static async Read(path){
+        try{
+            var dados = await fs.readFile(path, {encoding:"UTF-8"})
+            return dados
+        }catch(err){
+            console.error(err)
+        }
+    }
+}
+
+module.exports = Reader
